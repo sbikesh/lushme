91ad9787c1f731100746276b64d923ede114900e
bikesh shrestha <charliebikesh21@gmail.com>
2024-06-10 00:43:51 +0545

    web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3455321582"
+    serviceWorkerVersion: "3772499411"
   }
 });
